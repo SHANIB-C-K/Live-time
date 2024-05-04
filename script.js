512633514da6dde134318404586296698485e000
SHANIB-C-K <ckshanib8@gmail.com>
2024-05-04 11:28:40 +0530

    project is almost completed

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let date = new Date();
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
-
 function UpdateTime() {
-    
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  hours = hours < 10 ? "0" : hours;
+  minutes = minutes < 10 ? "0" : minutes;
+  seconds = seconds < 10 ? "0" : seconds;
+
+  let setTime = hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("time").innerHTML = setTime;
 }
+
+setInterval(UpdateTime, 1000);
+
+UpdateTime();
